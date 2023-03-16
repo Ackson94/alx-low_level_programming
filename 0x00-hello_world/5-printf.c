b1ec2207530fb9940f8149864103c61e7cbb2ba1
Ackson94 <acksonicmutuma@gmail.com>
2023-03-16 15:04:30 -0700

    C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - to display a proper grammer
+ *
+ * return: 0 when is successful
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
