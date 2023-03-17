8b51e0ed1e94e97a7705053689745fecb33bb1c6
Ackson94 <acksonicmutuma@gmail.com>
2023-03-16 17:26:28 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various variable types
+ * main - Prints the size of various types
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of char: %ld byte(s)\n", sizeof(char));
+printf("Size of int: %ld byte(s)\n", sizeof(int));
+printf("Size of long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of float: %ld byte(s)\n", sizeof(float));
+printf("Size of double: %ld byte(s)\n", sizeof(double));
+printf("Size of long double: %ld byte(s)\n", sizeof(long double));
 return (0);
 }
-
