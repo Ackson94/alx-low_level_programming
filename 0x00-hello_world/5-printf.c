720803f4109ce7d30a351d7c6115bf26bab79834
Ackson94 <acksonicmutuma@gmail.com>
2023-03-16 17:10:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - to display a proper grammer
- *
- * return: 0 when is successful
+ * main - prints the proper grammar
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
