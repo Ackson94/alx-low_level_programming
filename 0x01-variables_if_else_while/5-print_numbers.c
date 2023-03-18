bdaec357c74e629bca0ccd15ef68052ed84bf959
Ackson94 <acksonicmutuma@gmail.com>
2023-03-18 03:14:16 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all single digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+
+return (0);
+}
