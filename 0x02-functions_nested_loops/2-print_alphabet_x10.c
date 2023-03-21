967e5708b364e55d0e30d17a61e7e35d8f6dbfd0
Ackson94 <acksonicmutuma@gmail.com>
2023-03-21 15:44:57 -0700

    initals commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - to display an alphabet_x10 letters
+ */
+
+void print_alphabet_x10(void)
+{
+char i;
+int a;
+
+for (a = 1; a <= 10; a++)
+{
+
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(a);
+_putchar('\n');
+}
+
+}
+
+}
+
