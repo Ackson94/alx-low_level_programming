442cfda665f1dc756f069adcf307d5509b639d16
Ackson94 <acksonicmutuma@gmail.com>
2023-03-22 12:31:44 -0700

    update

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,27 +2,24 @@
 
 /**
  * main - the first 10 terms 
- * Return: Always 0 success
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 int i = 0;
-long j = 1;
-long a = 2; 
-long sum = a;
+long j = 1, a = 2, sum = a;
 
 while (a + j < 4000000)
 {
 a += j;
 
 if (a % 2 == 0)
-{
 sum += a;
 j = a - j;
 ++i;
-}
 }
 
 printf("%ld\n", sum);
 return (0);
 }
+
