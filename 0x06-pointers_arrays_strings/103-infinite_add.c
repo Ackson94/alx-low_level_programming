6c9a532f8a7f455a50cbceab40af8d3b1a3b9a38
Ackson94 <acksonicmutuma@gmail.com>
2023-03-29 14:00:51 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,65 +11,45 @@
 *
 * Return: Pointer to the result, or 0 if the result can not be stored in r
 */
-void number(char *n)
-{
-int a = 0;
-int b = 0;
-char value;
-
-while (*(n + a) != '\0')
-{
-a++;
-}
-a++;
-for (b = 0; b < a; b++, a--)
-{
-value = *(n + b);
-*(n + b) = *(n + a);
-*(n + a) = value;
-}
-}
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int a = 0, i = 0, j = 0, dights = 0;
-int b1 = 0, va2 = 0, tb = 0;
+int len1, len2, carry, i, j;
+char *p, *q;
 
-while (*(n1 + i) != '\0')
-i++;
-while (*(n2 + j) != '\0')
-j++;
-i--;
-j--;
+len1 = strlen(n1);
+len2 = strlen(n2);
 
-if (j >= size_r || i >= size_r)
+if (len1 > size_r || len2 > size_r)
 return (0);
 
-while (j >= 0 || i >= 0 || a == 1)
+p = n1 + len1 - 1;
+q = n2 + len2 - 1;
+
+carry = 0;
+for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--)
 {
-if (i < 0)
-b1 = 0;
-else
-b1 = *(n1 + i) - '0';
-if (j < 0)
-va2 = 0;
-else
-va2 = *(n2 + j) - '0';
-tb = b1 + va2 + a;
-if (tb >= 10)
-a = 1;
-else
-a = 0;
-if (dights >= (size_r - 1))
+int digit1 = i >= 0 ? *(p--) - '0' : 0;
+int digit2 = j >= 0 ? *(q--) - '0' : 0;
+int sum = digit1 + digit2 + carry;
+
+if (r - p - 1 > size_r || sum > 9)
 return (0);
-*(r + dights) = (tb % 10) + '0';
-dights++;
-j--;
-i--;
+
+carry = sum / 10;
+*(r++) = (sum % 10) + '0';
 }
-if (dights == size_r)
+
+*(r--) = '\0';
+
+while (r > n1 && *r == '0')
+r--;
+
+if (r - n1 + 1 > size_r)
 return (0);
-*(r + dights) = '\0';
-number(r);
-return (r);
+
+for (i = 0; i <= r - n1; i++)
+*(n1 + i) = *(r - i);
+
+return (n1);
 }
+
