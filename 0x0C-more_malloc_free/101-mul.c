4bf4d31756f05bfca5b3de821df50e93709a934e
Ackson Mutuma <78912538+Ackson94@users.noreply.github.com>
2023-04-13 20:25:48 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,64 +1,99 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * validate_input - validates the input argument
- * @num: input string to be validated
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: the integer value of the string
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int validate_input(char *num)
+int is_digit(char *s)
 {
-    int i;
+	int i = 0;
 
-    for (i = 0; i < strlen(num); i++)
-    {
-        if (!isdigit(num[i]))
-        {
-            printf("Error\n");
-            exit(98);
-        }
-    }
-    return (atoi(num));
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
- * multiply - multiplies two integers
- * @num1: first integer
- * @num2: second integer
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: the product of num1 and num2
+ * Return: the length of the string
  */
-int multiply(int num1, int num2)
+int _strlen(char *s)
 {
-    return (num1 * num2);
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
 }
 
 /**
  * main - multiplies two positive numbers
- * @argc: number of arguments passed to the program
- * @argv: array of pointers to the arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0 on success, 98 on failure
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (98);
-    }
-
-    num1 = validate_input(argv[1]);
-    num2 = validate_input(argv[2]);
-
-    result = multiply(num1, num2);
-
-    printf("%d\n", result);
-
-    return (0);
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
